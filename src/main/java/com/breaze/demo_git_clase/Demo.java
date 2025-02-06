5c67d99a2c734c376c74c7b2011756e5d920fee4
breaze <simon.gaviriao@gmail.com>
2025-02-06 17:52:23 -0500

    Main class was modified to just say hello and demo class was added

--- src/main/java/com/breaze/demo_git_clase/Demo.java
+++ src/main/java/com/breaze/demo_git_clase/Demo.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.breaze.demo_git_clase;
 
 /**
  *
  * @author breaze
  */
-public class Demo_git_clase {
-
-    public static void main(String[] args) {
-        System.out.println("World!");
-    }
+public class Demo {
+    
 }
