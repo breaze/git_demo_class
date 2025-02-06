ccaed791b4e5c841d7a7b9891e08806a261c89f1
breaze <simon.gaviriao@gmail.com>
2025-02-06 17:53:47 -0500

    Revert "Main class was modified to just say hello and demo class was added"
    This reverts commit 5c67d99a2c734c376c74c7b2011756e5d920fee4.

--- src/main/java/com/breaze/demo_git_clase/Demo_git_clase.java
+++ src/main/java/com/breaze/demo_git_clase/Demo_git_clase.java
@@ -11,6 +11,6 @@ package com.breaze.demo_git_clase;
 public class Demo_git_clase {
 
     public static void main(String[] args) {
-        System.out.println("World!");
+        System.out.println("Hello World!");
     }
 }
